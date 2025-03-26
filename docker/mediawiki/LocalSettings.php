e92bcc8657664be1f9e4c68005ad5fd219b57b51
Benjamin Aufdenberg <benaufdenberg@gmail.com>
2025-03-26 18:20:29 -0400

    3.21 18:20

--- docker/mediawiki/LocalSettings.php
+++ docker/mediawiki/LocalSettings.php
@@ -1,26 +1,24 @@
-﻿@"
-<?php
+﻿<?php
 # MediaWiki Local Settings
 wfLoadExtension( 'Scribunto' );
-`$wgScribuntoDefaultEngine = 'luastandalone';
-`$wgScribuntoEngineConf['luastandalone']['luaPath'] = '/usr/bin/lua5.1';
+$wgScribuntoDefaultEngine = 'luastandalone';
+$wgScribuntoEngineConf['luastandalone']['luaPath'] = '/usr/bin/lua5.1';
 
 # Allow API access
-`$wgEnableAPI = true;
-`$wgEnableWriteAPI = true;
-`$wgCrossSiteAJAXdomains = ['*'];
+$wgEnableAPI = true;
+$wgEnableWriteAPI = true;
+$wgCrossSiteAJAXdomains = ['*'];
 
 # Set up namespaces for templates and modules
-`$wgExtraNamespaces[10] = 'Template';
-`$wgExtraNamespaces[828] = 'Module';
+$wgExtraNamespaces[10] = 'Template';
+$wgExtraNamespaces[828] = 'Module';
 
 # Configure for Wiktionary-like processing
-`$wgNamespacesWithSubpages[NS_MAIN] = true;
-`$wgAllowExternalImages = true;
+$wgNamespacesWithSubpages[NS_MAIN] = true;
+$wgAllowExternalImages = true;
 
 # Increase limits for processing complex templates
-`$wgMaxArticleSize = 10485760; # 10MB
-`$wgMaxPPNodeCount = 1000000;
-`$wgMaxTemplateDepth = 100;
-`$wgMaxPPExpandDepth = 100;
-"@ | Out-File -FilePath "C:\Users\benau\wiktionary-db\docker\mediawiki\LocalSettings.php" -Encoding utf8
+$wgMaxArticleSize = 10485760; # 10MB
+$wgMaxPPNodeCount = 1000000;
+$wgMaxTemplateDepth = 100;
+$wgMaxPPExpandDepth = 100;
