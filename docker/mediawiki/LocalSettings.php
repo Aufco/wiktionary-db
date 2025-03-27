f786c1af5ee661aec407e110086196a7354ee145
Benjamin Aufdenberg <benaufdenberg@gmail.com>
2025-03-27 03:40:09 -0400

    3.22 03:39

--- docker/mediawiki/LocalSettings.php
+++ docker/mediawiki/LocalSettings.php
@@ -1,8 +1,15 @@
 ﻿<?php
-# MediaWiki Local Settings
-wfLoadExtension( 'Scribunto' );
-$wgScribuntoDefaultEngine = 'luastandalone';
-$wgScribuntoEngineConf['luastandalone']['luaPath'] = '/usr/bin/lua5.1';
+# Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "db";
+$wgDBname = "wiki_db";
+$wgDBuser = "wikiuser";
+$wgDBpassword = "wikipass";
+
+# Site settings
+$wgSitename = "Wiktionary Processor";
+$wgScriptPath = "";
+$wgServer = "http://localhost:8080";
 
 # Allow API access
 $wgEnableAPI = true;
@@ -22,3 +29,16 @@ $wgMaxArticleSize = 10485760; # 10MB
 $wgMaxPPNodeCount = 1000000;
 $wgMaxTemplateDepth = 100;
 $wgMaxPPExpandDepth = 100;
+
+# Scribunto settings
+wfLoadExtension('Scribunto');
+$wgScribuntoDefaultEngine = 'luastandalone';
+$wgScribuntoEngineConf['luastandalone']['luaPath'] = '/usr/bin/lua5.1';
+
+# Debugging
+$wgShowExceptionDetails = true;
+$wgShowDBErrorBacktrace = true;
+$wgDebugLogFile = "/var/www/html/debug.log";
+
+# Required at the end
+$wgUpgradeKey = "abc123";
